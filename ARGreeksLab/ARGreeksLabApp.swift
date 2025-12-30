e371697f70984bcc07b43c32f41673b6394c17e1
Rhea Shah <codingrhea@gmail.com>
2025-12-29 18:57:03 -0600

    Initial Commit

---
+++ ARGreeksLab/ARGreeksLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARGreeksLabApp.swift
+//  ARGreeksLab
+//
+//  Created by Rhea Shah on 12/29/25.
+//
+
+import SwiftUI
+
+@main
+struct ARGreeksLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
